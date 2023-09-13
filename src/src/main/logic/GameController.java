7758a1c3c13e8756d66ee7eef3148e050bac1a62
Sebastian Franz <sebastian.franz1@fh-aachen.de>
2023-09-13 14:27:31 +0200

    fill GameController

--- src/src/main/logic/GameController.java
+++ src/src/main/logic/GameController.java
@@ -1,11 +1,27 @@
 package main.logic;
 
 import main.data.Board;
+import main.data.Exceptions.invalidMoveException;
+
 //Class to start a new game and control the game flow
 public class GameController {
     Board currentBoard;
+    byte currentPlayer;
 
+    public GameController() {
+        startNewGame();
+    }
     private void startNewGame() {
         currentBoard = new Board();
+        currentPlayer = 1;
         }
+    public void makeMove(int x, int y) throws invalidMoveException {
+        currentBoard.setTile(x, y, currentPlayer);
+        if(currentPlayer == 1) {
+            currentPlayer = 2;
+        }
+        else {
+            currentPlayer = 1;
+        }
+    }
 }
