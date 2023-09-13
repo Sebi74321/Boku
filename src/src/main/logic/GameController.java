7f8515891d4be9a4213fc313172b4a702189a5e1
Sebastian Franz <sebastian.franz1@fh-aachen.de>
2023-09-13 14:24:25 +0200

    add GameContoller to start new games and control the game flow

---
+++ src/src/main/logic/GameController.java
@@ -0,0 +1,11 @@
+package main.logic;
+
+import main.data.Board;
+//Class to start a new game and control the game flow
+public class GameController {
+    Board currentBoard;
+
+    private void startNewGame() {
+        currentBoard = new Board();
+        }
+}
