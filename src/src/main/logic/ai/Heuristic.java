11a20298d0b2e8c706545e9475b68ff7f3420227
Sebastian Franz <sebastian.franz1@fh-aachen.de>
2023-10-23 20:51:56 +0200

    getPossibleCaptures

--- src/src/main/logic/ai/Heuristic.java
+++ src/src/main/logic/ai/Heuristic.java
@@ -3,27 +3,65 @@ package main.logic.ai;
 public class Heuristic {
 
     int score = 0;
-    int opponent ;
+    int player;
+    int opponent;
     int playerPieces = 0;
     int opponentPieces = 0;
-    int playerBlocked = 0;
-    int opponentBlocked = 0;
-    int playerMoves = 0;
-    int opponentMoves = 0;
-    int playerCaptures = 0;
-    int opponentCaptures = 0;
-    int playerCaptured = 0;
-    int opponentCaptured = 0;
-    int playerCapturedBlocked = 0;
-    int opponentCapturedBlocked = 0;
-    int playerCapturedMoves = 0;
-    int opponentCapturedMoves = 0;
-    public int evaluate(int[][] board, int player) {
+    boolean captureMoveMade = false;
+    int possibleOpponentCaptures = 0;
+
+    public Heuristic(int player) {
+        this.player = player;
+    }
+
+    public int evaluate(int[][] board) {
         opponent = player == 1 ? 2 : 1;
 
         getCurrentPieces(board);
+        getPossibleCaptures(board);
+
+
 
         return score;
+    }
+
+    private void getPossibleCaptures(int[][] board) {
+        //find pairs of 2 own pieces with 1 opponents piece one either side
+
+        //horizontal, vertical and diagonal
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                //horizontal
+                if(j<8){
+                    if (board[i][j]==player && board[i][j+1]==player && board[i][j+2]==opponent){
+                        possibleOpponentCaptures++;
+                    }
+                    if (board[i][j]==opponent && board[i][j+1]==player && board[i][j+2]==player){
+                        possibleOpponentCaptures++;
+                    }
+                }
+                //vertical
+                if(i<8){
+                    if (board[i][j]==player && board[i+1][j]==player && board[i+2][j]==opponent){
+                        possibleOpponentCaptures++;
+                    }
+                    if (board[i][j]==opponent && board[i+1][j]==player && board[i+2][j]==player){
+                        possibleOpponentCaptures++;
+                    }
+                    //diagonal
+                    if(j<8){
+                        if (board[i][j]==player && board[i+1][j+1]==player && board[i+2][j+2]==opponent){
+                            possibleOpponentCaptures++;
+                        }
+                        if (board[i][j]==opponent && board[i+1][j+1]==player && board[i+2][j+2]==player){
+                            possibleOpponentCaptures++;
+                        }
+                    }
+
+                }
+            }
+        }
+
     }
 
     private void getCurrentPieces(int[][] board) {
@@ -33,6 +71,9 @@ public class Heuristic {
                     playerPieces++;
                 } else if (board[i][j]==2){
                     opponentPieces++;
+                }
+                else if(board[i][j]==9){
+                    captureMoveMade = true;
                 }
             }
         }
